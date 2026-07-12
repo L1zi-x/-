0b58e707d07f17b95a95975cfc68d9ca6158336b
L1zi-x <lzx1205237865@gmail.com>
2026-07-12 20:51:15 +0800

    第一个独立编写的代码，用于英制单位与厘米之间的换算

--- hello.c
+++ hello.c
@@ -5,10 +5,10 @@ int main ()
 int a;
 int b;
  
-	printf("Ó¢³ß");
+	printf("foot");
 	scanf("%d", &a);
 	
-	printf("Ó¢´ç");
+	printf("inch");
 	scanf("%d",&b);
 		
 	printf("(%d + %d/12) * 0.3048= %f\n",a ,b ,(a + b/12.0) * 0.3048);
