a33c2d38675fee1e157000c82e17907c87280490
L1zi-x <lzx1205237865@gmail.com>
2026-07-13 08:13:20 +0800

    Calculate the time difference
    新的作业，用于计算时间差

---
+++ 260713.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int hour1,minute1;
+    int hour2,minute2;
+    
+    printf("input hour and minute\n");
+    
+    scanf("%d %d",&hour1,&minute1);
+    
+    printf("Enter new input hour and minute\n");
+    
+    scanf("%d %d",&hour2,&minute2);
+    
+    int total=abs((hour1*60 + minute1)-(hour2*60 + minute2));
+    
+	printf("%d minute,%f hour \n",total,(double)total/60);
+
+return 0;
+}
